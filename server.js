8f1346d305403a4b2e201ebddf1f7b3102e74fc1
figus <figus@users.noreply.github.com>
2018-08-03 01:22:46 +1000

    cambio a https

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
+const fs = require('fs');
+const https = require('https');
 const webpackConfig = require('./webpack.config.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`App is listening on port ${port}`);
+// });
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
+
+
+
 let compiler = webpack(webpackConfig);
 
 app.use(
@@ -23,3 +28,11 @@ app.use(
 );
 app.use(require('webpack-hot-middleware')(compiler));
 app.use(express.static(path.resolve(__dirname, 'dist')));
+
+https.createServer({
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.cert'),
+}, app)
+  .listen(3000, () => {
+    console.log('App listening on 3000')
+  });
