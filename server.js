d7a455b0029e280b33c8993958b5d0b611e38d36
figus <figus@users.noreply.github.com>
2018-10-20 00:46:56 +1100

    correccion para el refresh en otras paginas

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, 'dist')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
 
 app.use(
   require('webpack-dev-middleware')(compiler, {
@@ -24,10 +17,17 @@ app.use(
     publicPath: webpackConfig.output.publicPath,
     stats: { colors: true }
   })
-);
-app.use(require('webpack-hot-middleware')(compiler));
-
-https.createServer({
+  );
+  app.use(require('webpack-hot-middleware')(compiler));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  });
+  
+  https.createServer({
   key: fs.readFileSync('./server.key'),
   cert: fs.readFileSync('./server.cert'),
 }, app)
