c4884e7cc0c2556b1b968bdfc6d9b760ce1af4e7
figus <figus@users.noreply.github.com>
2018-10-24 01:45:39 +1100

    Busca por coles y woolworths y las mezcla

--- src/app/Api/Products.tsx
+++ src/app/Api/Products.tsx
@@ -1,13 +1,71 @@
 import IProduct from './../interfaces/IProduct';
 import { IWoolworthsResult, IProductInner, IProductOuter } from './../interfaces/IWoolworthsResult';
+import { IColesResult, ICatalogEntryView } from '../interfaces/IColesResult';
+import ICard from '../interfaces/ICard';
 
-export const productSearchAsync = async (term: string): Promise<IProduct[]> =>
+export const productSearchAsync = async (term: string): Promise<ICard[]> =>
 {
   const isBarcode = term.match(/^\d+$/gm) ? true : false;
 
   const wResults = await searchWoolworthsAsync(term, isBarcode);
+  const cResults = await searchColesAsync(term, isBarcode);
+  const results = wResults.concat(cResults);
 
-  return wResults;
+  const tarjetas: ICard[] = mergeResults(results);
+
+  return tarjetas;
+};
+
+export const mergeResults = (resultados: IProduct[]): ICard[] =>
+{
+  if (resultados.length <= 0)
+  {
+    return;
+  }
+
+  const tarjetas: ICard[] = [];
+
+  while (resultados.length > 0)
+  {
+    const currentProduct: IProduct = resultados.shift();
+    const tarjeta: ICard = {
+      barcode: currentProduct.barcode,
+      name: currentProduct.name,
+      imageUrl: currentProduct.image,
+      size: currentProduct.packageSize,
+      prices: [{
+        price: currentProduct.price,
+        cupString: currentProduct.cupString,
+        store: currentProduct.origin,
+      }],
+    };
+
+    if (resultados.length > 0)
+    {
+      const sameBc = resultados.filter((item) =>
+      {
+        return currentProduct.barcode === item.barcode;
+      });
+
+      resultados = resultados.filter((item) =>
+      {
+        return currentProduct.barcode !== item.barcode;
+      });
+
+      sameBc.forEach((item) =>
+      {
+        tarjeta.prices.push({
+          price: item.price,
+          cupString: item.cupString,
+          store: item.origin,
+        });
+      });
+    }
+
+    tarjetas.push(tarjeta);
+  }
+
+  return tarjetas;
 };
 
 export const searchWoolworthsAsync = async (term: string, barcode: boolean = false): Promise<IProduct[]> =>
@@ -54,7 +112,7 @@ export const searchWoolworthsAsync = async (term: string, barcode: boolean = fal
             price: productInner.Price,
             packageSize: productInner.PackageSize,
             onSale: productInner.IsOnSpecial,
-            origin: 'woolworths',
+            origin: 'W',
           });
         }
       });
@@ -67,8 +125,48 @@ export const searchWoolworthsAsync = async (term: string, barcode: boolean = fal
     throw new Error('Error al buscar producto: ' + err);
   }
 };
+export const searchColesAsync = async (term: string, barcode: boolean = false): Promise<IProduct[]> =>
+{
+  if (barcode && !term.match(/\d+/gm))
+  {
+    throw new Error(`${term} is not a barcode`);
+  }
 
-export const barcodeListSearch = (barcode: string[]): Array<Promise<IProduct>> =>
+  const proxy1 = 'https://thingproxy.freeboard.io/fetch/';
+  const queryUrl = proxy1 + 'https://shop.coles.com.au/search/resources/store/20501/productview/bySearchTerm/' + term;
+
+  try
+  {
+    const resultados = await fetch(queryUrl, {
+      method: 'GET',
+    });
+    const products: IProduct[] = [];
+
+    const productsRaw: IColesResult = await resultados.json();
+    productsRaw.catalogEntryView.map((product: ICatalogEntryView) =>
+    {
+      products.push({
+        barcode: term,
+        hasCupString: true,
+        cupString: product.u2.toUpperCase().replace('PER', ' / '),
+        image: 'https://shop.coles.com.au' + product.t,
+        name: product.n,
+        price: Number(product.p1.o),
+        packageSize: product.a.O3[0].toLowerCase(),
+        onSale: false,
+        origin: 'C',
+      });
+    });
+
+    return products;
+  }
+  catch (err)
+  {
+    throw new Error('Error al buscar producto: ' + err);
+  }
+};
+
+export const barcodeListSearch = (barcode: string[]): Array<Promise<ICard>> =>
 {
   return barcode.map(async (bc: string) =>
   {
