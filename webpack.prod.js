6302bb3f9f32d109a90920bfd18c1c73322e0357
figus <figus@users.noreply.github.com>
2018-10-27 01:42:25 +1100

    configuraciones

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const fs = require('fs');
 const plugins = [];
 
 module.exports = require('./webpack.base')({
   mode: 'production',
   devServer: {
     port: 3000,
-    contentBase: path.join(process.cwd(), 'dist/')
+    contentBase: path.join(process.cwd(), 'dist/'),
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'server', 'cert', 'localhost+3-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'server', 'cert', 'localhost+3.pem')),
+    },
+    historyApiFallback: true,
+    host: '0.0.0.0',
   },
   plugins
 });
